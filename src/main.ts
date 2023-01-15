d7c8c44059c20fe571c3127c873fe94122f108f4
markovaleksandr108@gmail.com <markovaleksandr108@gmail.com>
2023-01-16 00:59:46 +0200

    Added dependence. Now state stored in Vuex.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,25 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import { createStore } from 'vuex';
+
+const store = createStore({
+  state() {
+    return {
+      string: ''
+    }
+  },
+  mutations: {
+    refresh(state, value) {
+      state.string = value
+    }
+  },
+  getters: {
+    stringLength(state) {
+      return store.state.string.length
+    }
+  }
+})
 
 const app = createApp(App);
+app.use(store);
 app.mount("#app");
